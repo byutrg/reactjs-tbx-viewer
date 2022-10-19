210991f792d0e154ca538e955e856096b562569f
James Hayes <james.s.hayes@gmail.com>
2022-10-18 20:36:31 -0600

    remove unnecessary quotes in config.js

--- public/config.js
+++ public/config.js
@@ -1,16 +1,16 @@
 var configData = {
-        "backgroundColors": {
-            "checkboxColor": null, 
-            "checkboxCheckedColor": null, 
-            "conceptEntryBlockColor": null, 
-            "headerColor": null, 
-            "metadataButtonColor": null, 
-            "metadataTermBlinkActiveColor": null, 
-            "popupColor": null, 
-            "termBlockColor": null, 
-            "termBlockLangButtonColor": null 
+        backgroundColors: {
+            checkboxColor: null, 
+            checkboxCheckedColor: null, 
+            conceptEntryBlockColor: null, 
+            headerColor: null, 
+            metadataButtonColor: null, 
+            metadataTermBlinkActiveColor: null, 
+            popupColor: null, 
+            termBlockColor: null, 
+            termBlockLangButtonColor: null 
         },
-        "font": {
-            "fontFamily": null
+        font: {
+            fontFamily: null
         }
     }
